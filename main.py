401f8577b439f6e87623b14caf85597ac55b2762
Mark Fassler <mfassler@users.noreply.github.com>
2021-01-14 17:50:12 +0900

    WiP:  first, minimal version of the auto-pilot

--- main.py
+++ main.py
@@ -5,22 +5,44 @@ if sys.version_info[0] < 3:
     raise Exception("Must be using Python 3")
 
 import signal
+import struct
+import socket
 import numpy as np
 import cv2
 
 from misc_utils import get_last_packet
 from misc_map_tools import make_map
+from misc_map_tools import COLOR_YELLOW, COLOR_BLACK
+from misc_map_tools import make_yellow_avoidance_area, make_black_avoidance_area
 
 from YDLidar import YDLidar
 
 PIXELS_PER_METER = 100
 
+
+MOAB_ADDR = ('192.168.0.69', 12346)
+
+
+def check_color(a, b):
+    return a[0] == b[0] and a[1] == b[1] and a[2] == b[2]
+
+
 class LidarWithVisual(YDLidar):
     def __init__(self, *args, **kwargs):
         super(LidarWithVisual, self).__init__(*args, **kwargs)
         self._a_map = make_map(800, 800, PIXELS_PER_METER)
+        make_yellow_avoidance_area(self._a_map, PIXELS_PER_METER)
+        make_black_avoidance_area(self._a_map, PIXELS_PER_METER)
         self.amap = np.copy(self._a_map)
         self.prev_angle = 0.0
+
+        self.closest_yellow_object = (500.0, None, None)  # will have the form of r,x,y
+        #self.closest_red_object = (500.0, None, None)  # will have the form of r,x,y
+        self.closest_black_object = (500.0, None, None)  # will have the form of r,x,y
+
+        self._sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self._sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        self._sock.bind(("0.0.0.0", 0))
 
 
     def parse_pc_packet(self, angle_start, angle_end, payload):
@@ -33,21 +55,61 @@ class LidarWithVisual(YDLidar):
         angleSpace = np.linspace(rad_start, rad_end, len(ranges))
 
         for i,theta in enumerate(angleSpace):
-            r = ranges[i]
-            x = r * np.sin(theta)
-            y = r * np.cos(theta)
+            r = ranges[i]  # real distance, meters
+            if r > 0.1:
+                x = r * np.sin(theta)  # real X, meters
+                y = r * np.cos(theta)  # real Y, meters
 
-            x_pixel = int(round(-x*PIXELS_PER_METER + 400))
-            y_pixel = int(round(y*PIXELS_PER_METER + 400))
+                x_pixel = int(round(-x*PIXELS_PER_METER + 400))
+                y_pixel = int(round(y*PIXELS_PER_METER + 400))
 
-            if x_pixel > 1 and x_pixel < 798:
-                if y_pixel > 1 and y_pixel < 798:
-                    self.amap[y_pixel, x_pixel] = 0,0,0
+                if x_pixel > 1 and x_pixel < 798:
+                    if y_pixel > 1 and y_pixel < 798:
+                        pixel_color = self.amap[y_pixel, x_pixel]
+                        if check_color(pixel_color, COLOR_YELLOW):
+                            if r < self.closest_yellow_object[0]:
+                                self.closest_yellow_object = (r, x, y)
+                            self.amap[y_pixel, x_pixel] = 0,0,255
+                        elif check_color(pixel_color, COLOR_BLACK):
+                            if r < self.closest_black_object[0]:
+                                self.closest_black_object = (r, x, y)
+                            self.amap[y_pixel, x_pixel] = 0,255,255
+                        else:
+                            self.amap[y_pixel, x_pixel] = 0,0,0
 
             if angle_start < self.prev_angle:
                 cv2.imshow('asdf', self.amap)
                 cv2.waitKey(1)
+
+                # Auto-pilot:
+                if self.closest_black_object[0] < 499.0:
+                    print('STOP')
+                    sbus_steering = 1024
+                    sbus_throttle = 1024
+
+                elif self.closest_yellow_object[0] < 499.0:
+                    r, x, y = self.closest_yellow_object
+                    if x < 0:  # object on right
+                        print('  <---- steer left')
+                        sbus_steering = 1024 - 200
+                        sbus_throttle = 1200
+                    else:  # object on left
+                        print('  steer right ---->')
+                        sbus_steering = 1024 + 200
+                        sbus_throttle = 1200
+
+                else:
+                    sbus_steering = 1024
+                    sbus_throttle = 1324
+
+                pkt = struct.pack('HH', sbus_steering, sbus_throttle)
+                self._sock.sendto(pkt, MOAB_ADDR)
+
+
+                # reset the map:
                 self.amap = np.copy(self._a_map)
+                self.closest_yellow_object = (500.0, None, None)
+                self.closest_black_object = (500.0, None, None)
 
             self.prev_angle = angle_start
 
--- misc_map_tools.py
+++ misc_map_tools.py
@@ -1,6 +1,10 @@
 
 import numpy as np
-import cv2 as cv
+import cv2
+
+
+COLOR_YELLOW = np.array([153,255,255])
+COLOR_BLACK = np.array([50,50,50])
 
 
 def make_map(width, height, grid_spacing):
@@ -16,18 +20,61 @@ def make_map(width, height, grid_spacing):
 
     # Draw the grid:
     for x in np.arange(grid_spacing, width, grid_spacing):
-        cv.line(_map, (x, 0), (x, height), (255,200,150), 2)
+        cv2.line(_map, (x, 0), (x, height), (255,200,150), 2)
     for y in np.arange(grid_spacing, height, grid_spacing):
-        cv.line(_map, (0, y), (width, y), (255,200,150), 2)
+        cv2.line(_map, (0, y), (width, y), (255,200,150), 2)
 
     # Radial gridlines:
     max_radius = np.sqrt(height**2 + (width/2)**2)
     x_center = int(round(width/2))
     y_center = int(round(height/2))
     for r in np.arange(grid_spacing, max_radius, grid_spacing):
-        cv.circle(_map, (x_center, y_center), int(r), (255,200,150), 2)
+        cv2.circle(_map, (x_center, y_center), int(r), (255,200,150), 2)
 
     # The position of the rover:
-    #cv.rectangle(_map, (x_center-25, height-10), (x_center+25, height), (0,0,0), -1)
+    #cv2.rectangle(_map, (x_center-25, height-10), (x_center+25, height), (0,0,0), -1)
     return _map
+
+
+
+
+
+def make_yellow_avoidance_area(img, PIXELS_PER_METER):
+    vehicle_width = 0.5  # meters, float
+    max_width = 1.0  # meters, float
+    max_distance = 2.0  # meters, float
+
+    # Convert these real, float values into pixels on the radar screen:
+    h_center = 400  # pixel center of img
+    v_center = 400  # pixel center of img
+
+    bottom_left = h_center - int(round(vehicle_width * PIXELS_PER_METER / 2))
+    bottom_right = h_center + int(round(vehicle_width * PIXELS_PER_METER / 2))
+
+    max_left = h_center - int(round(max_width * PIXELS_PER_METER / 2))
+    max_right = h_center + int(round(max_width * PIXELS_PER_METER / 2))
+
+    v_top = v_center - int(round(max_distance * PIXELS_PER_METER))
+
+    pts = np.array([
+        [bottom_left, v_center],
+        [max_left, 300],
+        [max_left, v_top],
+        [max_right, v_top],
+        [max_right, 300],
+        [bottom_right, v_center],
+    ], np.int32)
+    pts.reshape((-1, 1, 2))
+    cv2.fillPoly(img, [pts], (153, 255,255))  # TODO:  why can't I put "COLOR_YELLOW" here?
+
+
+def make_black_avoidance_area(img, PIXELS_PER_METER):
+    vehicle_width = 0.5  # meters, float
+    max_distance = 0.5 # meters, float
+    k_w = int(round(vehicle_width * PIXELS_PER_METER / 2))
+    k_h = int(round(max_distance * PIXELS_PER_METER))
+
+    cv2.ellipse(img, (400, 400), (k_w, k_h), 0, 0, -180, (50, 50, 50), -1)
+
+
 

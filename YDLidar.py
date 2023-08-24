b211692e9f882ee834d21b678d29e65753b27a0d
Mark Fassler <mfassler@users.noreply.github.com>
2023-08-24 13:48:55 +0900

    make it work better...   make the viewer be network-transparent (separate process via UDP)

--- YDLidar.py
+++ YDLidar.py
@@ -1,45 +1,74 @@
 #!/usr/bin/env python3
 
+import time
 import struct
+import socket
 import threading
 import serial
 
 
-def parse_single_response(mode, _type, payload):
-
-    if _type == 0x04:  # device information
-        model_number, firm_minor, firm_major, hard_version = struct.unpack('BBBB', payload[:4])
-        print('Firmware version: %d.%d' % (firm_major, firm_minor))
-        print('Hardware version: %d' % (hard_version))
-        print('Model:', model_number)
-        serNum = ''
-        for val in payload[4:]:
-            serNum += val.__str__()
-        print('Serial:', serNum)
-
-    elif _type == 0x06:  # device health
-        status, health = struct.unpack('<BH', payload)
-        if status == 0 and health == 0:
-            print('Health status is good.')
-        else:
-            print("Problem.  status_code: %x,  error_code: %x" % (status, health))
-
-
 class YDLidar:
     def __init__(self, device, baud):
+        self._sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        #self._sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        self._sock.bind(("0.0.0.0", 0))
+        self._DEST_ADDR = ('127.0.0.1', 25811)
+
         self._ser = serial.Serial(device, baud)
+
+        # Send stop command:
+        pkt = bytes([0xa5, 0x65])  # Stop scanning (and switch to idle mode)
+        self._ser.write(pkt)
+
+        # Wait for things to settle down:
+        time.sleep(0.01)
+        self._ser.read(self._ser.inWaiting())
+
+        self._running = False
+        self._t1 = None
+
+    def start(self):
         self._running = True
         self._t1 = threading.Thread(target=self.reading_task)
         self._t1.start()
+        pkt = bytes([0xa5, 0x60])  # Start scanning (and switch to scan mode)
+        self._ser.write(pkt)
 
-        # just for debugging, etc:
-        self._single_packets = []
-        self._continuous_packets = []
+    def stop(self):
+        pkt = bytes([0xa5, 0x65])  # Stop scanning (and switch to idle mode)
+        self._ser.write(pkt)
+        self._running = False
+        self._t1.join()
 
+    def set_desired_scan_frequency(self, desired_frequency: float):
+        # device can actually be set to higher frequencies...  prolly not good for it
+        assert desired_frequency <= 12.0, 'Requested scan frequency too high'
+        assert desired_frequency >= 5.0, 'Requested scan frequency too low'
+
+        freq = self.get_scan_frequency()
+        if freq is None:
+            time.sleep(0.1)
+            freq = self.get_scan_frequency()
+
+        for i in range(20):
+            err = freq - desired_frequency
+            if err < -1:
+                freq = self.increase_scan_freq_1hz()
+            elif err <= -0.09:
+                freq = self.increase_scan_freq_01hz()
+            elif err > 1:
+                freq = self.decrease_scan_freq_1hz()
+            elif err >= 0.09:
+                freq = self.decrease_scan_freq_01hz()
+            else:
+                break
+
+        return freq
 
     def reading_task(self):
-        #count = 0
-        #while count < 100:
+        lsn = None
+        fsa = None
+        lsa = None
         while self._running:
             c = self._ser.read(1)
 
@@ -48,20 +77,13 @@ class YDLidar:
                 c = self._ser.read(1)
                 if c == b'\x5a':
                     len_mode_type = self._ser.read(5)
-                    plen, = struct.unpack('I', len_mode_type[:4])
+                    plen, = struct.unpack('<I', len_mode_type[:4])
                     plen &= 0x3fffffff
                     mode = (len_mode_type[3] & 0xc0) >> 6
-
-                    # mode 1 means continuous response, so the single-response payload is ignored
-                    if mode == 1:
-                        plen = 0
-
                     _type = len_mode_type[4]
-                    print("plen:", plen, "mode:", mode, "type:", _type)
-
+                    print('WARNING:  received a single-response packet during continuous mode')
+                    print('plen:', plen, 'mode:', mode, 'type:', _type)
                     payload = self._ser.read(plen)
-                    self._single_packets.append(  (len_mode_type, payload) )
-                    parse_single_response(mode, _type, payload)
 
                 else:  # invalid 2nd char
                     print('parser miss (expecting 0x5a):', c)
@@ -71,31 +93,64 @@ class YDLidar:
                 c = self._ser.read(1)
                 if c == b'\x55':
                     ct, lsn, fsa, lsa, cs = struct.unpack('<BBHHH', self._ser.read(8))
-                    pc_data = self._ser.read(lsn*2)
-                    #self._continuous_packets.append( (ct, lsn, fsa, lsa, cs, pc_data) )
-                    #count += 1
-                    if ct == 0:
+                    pc_data = self._ser.read(lsn * 2)
+                    if ct & 0x01 == 0:
                         self.parse_pc_packet(fsa, lsa, pc_data)
+                        #if fsa <= 13800 and lsa >= 13800:
+                        #    nothing = self._ser.read(1)
+                    else:
+                        self.actual_scan_frequency = (((ct & 0xfe) >> 1) + 30) / 10.0
+                        #print('freq:', self.actual_scan_frequency)
 
                 else:  # invalid 2nd char
                     print('parser miss (expecting 0x55):', c)
 
             else:  # invalid 1st char
-                print('parser miss (expecting either 0xa5 or 0xaa):', c)
+                # We always seem to get an extra byte around lsa ~= 13800... wtf...
+                print('parser miss (expecting either 0xa5 or 0xaa):', c, 'lsn:', lsn, fsa, lsa)
 
-        if not self._running:
-            self._thread_quit()
+    def send_then_get(self, pkt):
+        # empty out the RX buffer
+        print('send then get...', self._ser.inWaiting())
+        nothing = self._ser.read(self._ser.inWaiting())
 
-
-    def parse_pc_packet(self, angle_start, angle_end, payload):
-        pass
-
-    def _thread_quit(self):
-        pass
-
-    def stop(self):
-        pkt = bytes([0xa5, 0x65])
         self._ser.write(pkt)
+        c = self._ser.read(1)
+        if c == b'\xa5':
+            c = self._ser.read(1)
+            if c == b'\x5a':
+                len_mode_type = self._ser.read(5)
+                plen, = struct.unpack('<I', len_mode_type[:4])
+                plen &= 0x3fffffff
+                mode = (len_mode_type[3] & 0xc0) >> 6
+                if mode == 1:
+                    print('WARNING:  received a continuous-response packet during idle mode')
+                _type = len_mode_type[4]
+                payload = self._ser.read(plen)
+                return mode, _type, payload
+            else:
+                print('unknown 2nd byte:', c)
+        else:
+            print('unknown start byte:', c)
+
+        print('WARNING:  didn\'t parse anything...  bytes:', self._ser.inWaiting())
+        return None, None, None
+
+    def parse_pc_packet(self, fsa: int, lsa: int, pc_data: bytes):
+        '''
+        Parameters
+        ----------
+        fsa : int, start angle *128
+        lsa : int, start angle *128
+        pc_data : bytes, pointcloud data, array of 2-bytes, little-endian
+            usually, about 80 bytes (40 samples), I think
+        '''
+        pkt = struct.pack('<HH', fsa, lsa) + pc_data
+        self._sock.sendto(pkt, self._DEST_ADDR)
+
+    #def stop(self):
+    #    pkt = bytes([0xa5, 0x65])
+    #    self._ser.write(pkt)
 
     def force_stop(self):
         pkt = bytes([0xa5, 0x00])
@@ -103,26 +158,113 @@ class YDLidar:
 
     def get_device_info(self):
         pkt = bytes([0xa5, 0x90])
-        self._ser.write(pkt)
+        mode, _type, payload = self.send_then_get(pkt)
+        assert mode == 0
+        assert _type == 0x04
+        assert len(payload) == 0x14
+        model_number, firm_minor, firm_major, hard_version = struct.unpack('BBBB', payload[:4])
+        print('Firmware version: %d.%d' % (firm_major, firm_minor))
+        print('Hardware version: %d' % (hard_version))
+        known_models = {
+            100: 'TG15',
+            101: 'TG30',
+            102: 'TG50',
+        }
+        if model_number in known_models:
+            print(f'Model: {model_number} ({known_models[model_number]})')
+        else:
+            print(f'Model: {model_number}')
+        serNum = ''
+        for val in payload[4:]:
+            serNum += val.__str__()
+        print('Serial:', serNum)
 
     def get_device_health(self):
         pkt = bytes([0xa5, 0x92])
-        self._ser.write(pkt)
-
-    def get_sample_rate(self):
-        pkt = bytes([0xa5, 0xd1])
-        self._ser.write(pkt)
+        mode, _type, payload = self.send_then_get(pkt)
+        assert mode == 0
+        assert _type == 0x06
+        assert len(payload) == 3
+        status, health = struct.unpack('<BH', payload)
+        if status == 0 and health == 0:
+            print('Health status is good.')
+        else:
+            print("Problem.  status_code: %x,  error_code: %x" % (status, health))
 
     def scan(self):
         pkt = bytes([0xa5, 0x60])
         self._ser.write(pkt)
 
+    def _parse_scan_frequency(self, mode, _type, payload):
+        if mode == 0 and _type == 4 and len(payload) == 4:
+            val, = struct.unpack('<I', payload)
+            freq = val / 100.0
+            return freq
+        else:
+            print('FAILED to parse scan frequency')
 
-#lidar.get_sample_rate()
+    def increase_scan_freq_01hz(self):
+        '''
+        Increase scan frequency by 0.1 Hz
+        '''
+        pkt = bytes([0xa5, 0x09])
+        mode, _type, payload = self.send_then_get(pkt)
+        return self._parse_scan_frequency(mode, _type, payload)
 
-# 0xd1 -- get_sample_rate:
-# 0xd0 -- set_sample_rate:
-# 0x0d -- get_aimspeed:
-# 0x93 -- ??
-# 0x60 -- scan:
+    def decrease_scan_freq_01hz(self):
+        '''
+        Decrease scan frequency by 0.1 Hz
+        '''
+        pkt = bytes([0xa5, 0x0a])
+        mode, _type, payload = self.send_then_get(pkt)
+        return self._parse_scan_frequency(mode, _type, payload)
+
+    def increase_scan_freq_1hz(self):
+        '''
+        Increase scan frequency by 1 Hz
+        '''
+        print('a')
+        pkt = bytes([0xa5, 0x0b])
+        print('b')
+        mode, _type, payload = self.send_then_get(pkt)
+        print('c')
+        return self._parse_scan_frequency(mode, _type, payload)
+
+    def decrease_scan_freq_1hz(self):
+        '''
+        Increase scan frequency by 1 Hz
+        '''
+        pkt = bytes([0xa5, 0x0c])
+        mode, _type, payload = self.send_then_get(pkt)
+        return self._parse_scan_frequency(mode, _type, payload)
+
+    def get_scan_frequency(self):
+        '''
+        Get the Scan Frequency on the device.  (This is the set-point, not the actual)
+        '''
+        pkt = bytes([0xa5, 0x0d])
+        mode, _type, payload = self.send_then_get(pkt)
+        return self._parse_scan_frequency(mode, _type, payload)
+
+    def get_scan_frequency_async(self):
+        '''
+        Get the Scan Frequency on the device.  (This is the set-point, not the actual)
+        '''
+        pkt = bytes([0xa5, 0x0d])
+        self._ser.write(pkt)
+
+    def get_zero_angle_deviation(self):
+        self._commandQueue.append('Zero Angle Deviation')
+        pkt = bytes([0xa5, 0x93])
+        self._ser.write(pkt)
+
+
+if __name__ == '__main__':
+
+    #lidar = YDLidar('/dev/ttyUSB0', 500000)
+    lidar = YDLidar('/dev/ttyUSB0', 512000)
+    freq = lidar.set_desired_scan_frequency(11.5)
+    print(f'Current scan frequency: {freq} Hz')
+    #lidar.start()
+
 
---
+++ network_viewer.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+
+import sys
+import struct
+import socket
+import numpy as np
+import cv2
+
+from misc_map_tools import make_map
+
+PIXELS_PER_METER = 100
+
+
+class LidarViewer:
+    def __init__(self):
+        self._sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self._sock.bind(('127.0.0.1', 25811))
+        self._a_map = make_map(800, 800, PIXELS_PER_METER)
+        self.amap = np.copy(self._a_map)
+        self.prev_angle = 0.0
+
+    def run(self):
+        while True:
+            pkt, addr = self._sock.recvfrom(65535)
+            print('rx', len(pkt), 'bytes')
+
+            fsa, lsa = struct.unpack('<HH', pkt[:4])
+            pc_data = pkt[4:]
+
+            self.parse_pc_packet(fsa, lsa, pc_data)
+
+    def parse_pc_packet(self, angle_start, angle_end, payload):
+        print('parse_pc_packet')
+        rad_start = np.radians(angle_start / 128.0)
+        rad_end = np.radians(angle_end / 128.0)
+        if rad_end < rad_start:
+            rad_end += 2 * np.pi
+
+        ranges = np.frombuffer(payload, np.uint16) / 1000.0  # in meters
+        angleSpace = np.linspace(rad_start, rad_end, len(ranges))
+
+        for i, theta in enumerate(angleSpace):
+            r = ranges[i]
+            x = r * np.sin(theta)
+            y = r * np.cos(theta)
+
+            x_pixel = int(round(-x * PIXELS_PER_METER + 400))
+            y_pixel = int(round(y * PIXELS_PER_METER + 400))
+
+            if x_pixel > 1 and x_pixel < 798:
+                if y_pixel > 1 and y_pixel < 798:
+                    self.amap[y_pixel, x_pixel] = 0, 0, 0
+
+            if angle_start < self.prev_angle:
+                print('c', end='')
+                sys.stdout.flush()
+                cv2.imshow('asdf', self.amap)
+                print('c', end='')
+                sys.stdout.flush()
+                cv2.waitKey(1)
+                print('c', end='')
+                sys.stdout.flush()
+                self.amap = np.copy(self._a_map)
+
+            self.prev_angle = angle_start
+
+
+viewer = LidarViewer()
+viewer.run()
+
+
